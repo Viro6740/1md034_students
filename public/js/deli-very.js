a45634b9e6612d9547c48a881f25eab9ce189fba
viro6740 <viro6740@student.uu.se>
2019-02-11 17:35:59 +0100

    progress messaing

--- public/js/deli-very.js
+++ public/js/deli-very.js
@@ -5,34 +5,39 @@
 var socket = io();
 
 var vm = new Vue({
-  el: '#dots',
-  data: {
-    orders: {},
-  },
-  created: function () {
-    socket.on('initialize', function (data) {
-      this.orders = data.orders;
-    }.bind(this));
-
-    socket.on('currentQueue', function (data) {
-      this.orders = data.orders;
-    }.bind(this));
-  },
-  methods: {
-    getNext: function () {
-      var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
-        return Math.max(last, next);
-      }, 0);
-      return lastOrder + 1;
+    el: '#finalize',
+    data: {
+        orders: {},
+        target: {},
+        key: 'T',         
+        info: ""
     },
-    addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
-      socket.emit("addOrder", { orderId: this.getNext(),
-                                details: { x: event.clientX - 10 - offset.x,
-                                           y: event.clientY - 10 - offset.y },
-                                orderItems: ["Beans", "Curry"]
-                              });
+    methods: {
+        getNext: function () {
+            var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+                return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+        },
+        addOrder: function (event) {
+            var items = getContact().slice(4)
+            socket.emit("addOrder", { orderId: this.getNext(),
+                                      details: { x: this.target.x,
+                                                 y: this.target.y },
+                                      orderItems: items
+                            });
+        },
+        displayOrder: function (event) {          
+            var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                          y: event.currentTarget.getBoundingClientRect().top};          
+            this.target = { x: event.clientX - 10 - offset.x,
+                            y: event.clientY - 10 - offset.y }
+        },
+        
+        markDone: function(array) {
+            console.log(getContact())            
+            this.info = getContact()
+            
+        }   
     }
-  }
-});
+                });
